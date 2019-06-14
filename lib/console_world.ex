b49b891bd26ab014908fe9b5f14cbf89b1e8376a
Tessa Anderso <quintessa@8thlight.com>
2019-06-14 16:18:10 -0500

    returns two sizes of the board

--- lib/console_world.ex
+++ lib/console_world.ex
@@ -1,8 +1,12 @@
 defmodule ConsoleWorld do
-  def empty_world do
+  def empty_world(%{size: size}) do
+    if(size == 2) do
+"|-|-|
+|-|-|"
+    else
 "|-|-|-|
 |-|-|-|
 |-|-|-|"
+    end
   end
-
 end
--- test/console_world_test.exs
+++ test/console_world_test.exs
@@ -1,10 +1,17 @@
 defmodule ConsoleWorldTest do
   use ExUnit.Case
 
-  test "returns an empty world as text" do
-    assert ConsoleWorld.empty_world ==
+  test "returns an empty world of size 3 as text" do
+    assert ConsoleWorld.empty_world(%{size: 3}) ==
 "|-|-|-|
 |-|-|-|
 |-|-|-|"
   end
+
+  test "returns an empty world of size 2 as text" do
+    assert ConsoleWorld.empty_world(%{size: 2}) ==
+"|-|-|
+|-|-|"
+  end
+
 end
