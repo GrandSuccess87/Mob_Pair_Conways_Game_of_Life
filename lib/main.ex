0d0f182d163896fda2135e9aea5e3b5ab1a2411b
Tessa Anderso <quintessa@8thlight.com>
2019-06-17 18:22:29 -0500

    return one living cell

--- lib/main.ex
+++ lib/main.ex
@@ -1,1 +1,3 @@
-IO.puts(ConsoleWorld.empty_world(%{:size => 2}))
+IO.puts(ConsoleWorld.empty_world(%{:size => 15}))
+IO.puts(ConsoleWorld.populated_world(%{:size => 2}, %{world: 1}))
+IO.puts(ConsoleWorld.populated_world_first_generation(%{:size => 15}, %{world: 1}))
--- test/console_world_test.exs
+++ test/console_world_test.exs
@@ -3,20 +3,45 @@ defmodule ConsoleWorldTest do
 
   test "returns an empty world of size 3 as text" do
     assert ConsoleWorld.empty_world(%{size: 3}) ==
-["|-|-|-|
+"|-|-|-|
 |-|-|-|
-|-|-|-|"]
+|-|-|-|"
   end
 
   test "returns an empty world of size 2 as text" do
     assert ConsoleWorld.empty_world(%{size: 2}) ==
-["|-|-|
-|-|-|"]
+"|-|-|
+|-|-|"
   end
 
   test "returns an empty world of size 15 as text" do
     assert ConsoleWorld.empty_world(%{size: 15}) ==
 "|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|"
+  end
+
+  test "returns a world of size 2 with one living cell as text" do
+    assert ConsoleWorld.populated_world(%{size: 2}, %{world: 1}) ==
+"|*|-|
+|-|-|"
+  end
+
+  test "returns a world of size 15 with one living cell as text" do
+    assert ConsoleWorld.populated_world_first_generation(%{size: 15}, %{world: 1}) ==
+"|*|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
 |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
 |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
 |-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
